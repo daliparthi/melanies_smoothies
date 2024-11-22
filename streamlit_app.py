2ef7cdb7e7bed6ba43217ea13d49216ffdfad4ab
daliparthi <90120089+daliparthi@users.noreply.github.com>
2024-11-22 17:11:30 -0500

    Update streamlit_app.py
    fixing typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', each_fruit,' is ', search_on, '.')
+        #st.write('The search value for ', each_fruit,' is ', search_on, '.')
         st.subheader(each_fruit + ' Nutritition Information')
         smoothie_fruit_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothie_fruit_response.json(),use_container_width=True)
